d4c1e69dceb4467143a4ad9ebf162a4dd565ec49
cty012 <tc37@illinois.edu>
2025-03-06 13:25:04 -0600

    Add welcome mat

---
+++ scripts/controllers/welcome_mat_controller.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var event_system: EventSystem
+
+
+var index := 0
+
+
+func _on_body_entered(body: Node) -> void:
+    event_system.emit("game::switch-car", { "index": index })
+
+
+func _ready() -> void:
+    event_system = $/root/scene/event_system
+    body_entered.connect(_on_body_entered)
