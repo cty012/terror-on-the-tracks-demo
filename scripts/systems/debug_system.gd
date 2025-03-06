e056735d6a98d304680c41ae191830c9e56f66d5
iramo3 <iramo3@illinois.edu>
2025-03-06 13:36:32 -0600

    attempting merge main

--- scripts/systems/debug_system.gd
+++ scripts/systems/debug_system.gd
@@ -1,22 +1,16 @@
 extends Node
 
 var event_system: EventSystem
-var input_system: InputSystem
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    event_system = $/root/scene/event_system
-    input_system = $/root/scene/input_system
+	event_system = $/root/scene/event_system
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    if input_system.is_key_just_pressed(KEY_COMMA):
-        event_system.emit("game::switch-car", { "index": 0 })
-    elif input_system.is_key_just_pressed(KEY_PERIOD):
-        event_system.emit("game::switch-car", { "index": 1 })
-    elif input_system.is_key_just_pressed(KEY_EQUAL):
-        event_system.emit("game::sus-change", { "amount": 10 })
-    elif input_system.is_key_just_pressed(KEY_MINUS):
-        event_system.emit("game::sus-change", { "amount": -10 })
+	if Input.is_key_pressed(KEY_COMMA):
+		event_system.emit("game::switch-car", { "index": 0 })
+	elif Input.is_key_pressed(KEY_PERIOD):
+		event_system.emit("game::switch-car", { "index": 1 })
